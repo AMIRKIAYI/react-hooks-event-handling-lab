a8c59814c0069667a003366d8b7dd858df4e6a2a
AMIRKIAYI <amirkiayi516@gmail.com>
2024-06-30 14:54:18 +0300

    two files complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function handleFocus(){
+        console.log('Good!')
+    }
+    function handleBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+    return(
+        <button text='Eyes on me' onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+
+    )
+}
+export default EyesOnMe;
